35aa565f21d40652b8a01767293e1cb3f45223fc
Boopya <rikiyaeduarteyamamoto@gmail.com>
2020-02-26 15:11:05 +0800

    create interface for sql statements

--- app/src/main/java/com/example/minutetotapit/SQLStatements.java
+++ app/src/main/java/com/example/minutetotapit/SQLStatements.java
@@ -1,4 +1,24 @@
 package com.example.minutetotapit;
 
 public interface SQLStatements extends DatabaseConstants {
+    String CREATE_ENTRIES =
+        "CREATE TABLE " + TABLE_NAME + " (" +
+                USERNAME_COLUMN + " TEXT PRIMARY KEY, " +
+                PASSWORD_COLUMN + " TEXT, " +
+                SCORE_COLUMN + " INTEGER DEFAULT 0" +
+        ")";
+
+    String DELETE_ENTRIES =
+        "DROP TABLE IF EXISTS " + TABLE_NAME;
+
+    String QUERY_ENTRIES =
+        "SELECT * FROM " + TABLE_NAME + " WHERE " + USERNAME_COLUMN + " = ?";
+
+    String GET_USER_SCORE =
+        "SELECT MAX(" + SCORE_COLUMN + ") FROM " + TABLE_NAME + " WHERE " + USERNAME_COLUMN + "=?";
+
+    String CHECK_CREDENTIALS =
+        "SELECT * FROM " + TABLE_NAME + " WHERE " +
+            USERNAME_COLUMN + "=?" + " AND " +
+            PASSWORD_COLUMN + "=?";
 }
