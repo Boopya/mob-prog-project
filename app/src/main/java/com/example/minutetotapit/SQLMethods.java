2f812e6a599fa3ae7c4c535770c548bb705272d0
Boopya <rikiyaeduarteyamamoto@gmail.com>
2020-02-26 15:33:59 +0800

    delete SQLMethods interface

--- app/src/main/java/com/example/minutetotapit/SQLMethods.java
+++
@@ -1,9 +0,0 @@
-package com.example.minutetotapit;
-
-public interface SQLMethods {
-    boolean insertData(String username, String password);
-    boolean checkUsername(String username);
-    boolean isValidCredentials(String username, String password);
-    int getScore(String username);
-    void updateScore(String username, int score);
-}
